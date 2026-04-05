a4fa5a7266f952a94243079139db71f64bfaf68b
Heorhii Minesko <heorhii.minesko@gmail.com>
2026-04-05 12:11:48 +0300

    added the solution of the task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,22 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const notifMessage = document.createElement('div');
+  const notifTitle = document.createElement('h2');
+  const notifDescription = document.createElement('p');
+
+  notifMessage.classList.add('notification', type);
+  notifTitle.classList.add('title');
+  notifTitle.textContent = title;
+  notifDescription.textContent = description;
+  notifMessage.append(notifTitle, notifDescription);
+  notifMessage.style.top = `${posTop}px`;
+  notifMessage.style.right = `${posRight}px`;
+  document.body.appendChild(notifMessage);
+
+  setTimeout(() => {
+    notifMessage.style.visibility = 'hidden';
+  }, 2000);
 };
 
 pushNotification(
